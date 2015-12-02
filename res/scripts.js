17ce3afdb9fddb559cc1f8180e566da8c2ecfe81
Bruno Henriques <corruption1337@gmail.com>
2015-12-02 04:34:12 +0000

    Add hack injection and example scripts

--- res/scripts.js
+++ res/scripts.js
@@ -9,47 +9,98 @@ var BD = {
 var USER = null;	// shameless. must change dis
 
 
-notepadApp.controller("loginCtrl", function($scope){
-	$scope.loggedUser = null;
-	$scope.loggedStatus = "You are not logged in";
-
-	$scope.login = function(){
-		var user = $scope.bdusername;
-		var pass = $scope.bdpassword;
-		
-		$.ajax({
-			url: "req/login.php",
-			data: {user: user, pass: pass},
-			dataType: "json",
-			success: function(data){
-				console.log(data[0]);
-				if (data.length > 0){
-					$scope.loggedUser = data[0];
-					$scope.loggedStatus = "Logged in as " + $scope.loggedUser.nome;
-					USER = $scope.loggedUser;
-					$scope.$apply();
-					alert("Boas " + $scope.loggedUser.nome + "! :)");
-				} else{
-					$scope.loggedUser = null;
-					$scope.loggedStatus = "Login failed.";
-					USER = $scope.loggedUser;
-					$scope.$apply();
-				}
-			}
-		});
+notepadApp.service("log", function(){
+	this.isLogged = false;
+	this.loggedUser = null;
+	this.loggedStatus = "You are not logged in!";
+	
+	this.login = function(user){
+		this.isLogged = true;
+		this.loggedUser = user.nome;
+		this.loggedStatus = "Logged in as " + this.loggedUser + "...";
+	};
+	
+	this.fail = function(){
+		this.isLogged = false;
+		this.loggedUser = null;
+		this.loggedStatus = "Login failed!";
+	};
+	
+	this.getLogStatus = function(){
+		return this.loggedStatus;
 	};
 	
 });
 
-notepadApp.controller("pagesCtrl", function($scope){
+
+notepadApp.controller("loginCtrl", ["$scope", "$http", "log", function($scope, $http, log){
+	$scope.loggedStatus = log.loggedStatus;
+	
+	$scope.login = function(){
+		var user = $scope.bdusername;
+		var pass = $scope.bdpassword;
+		
+		$http.get("req/login.php", {params: {user: user, pass: pass}}).success(function(data){
+			console.log(data[0]);
+			if (data.length > 0){
+				log.login(data[0]);
+			} else{
+				log.fail();
+			}
+			$scope.loggedStatus = log.getLogStatus();
+		});
+	};
+
+	
+}]);
+
+notepadApp.controller("pagesCtrl", ["$scope", "$http", "log", function($scope, $http, log){
 	$scope.pages = BD["paginas"];
-});
+	//$scope.userID = log.loggedUser.userid;
+	
+	//if ($scope.userID){
+		$http.get("req/hack.php", {params: {query: "SELECT * FROM pagina WHERE userid=43"}}).success(function(data){
+			console.log(data);
+			if (data.length > 0){
+				$scope.pages = data;
+			} else{
+				$scope.pages = BD["paginas"];
+			}
+		});
+	//}
+	
+}]);
 
 
-notepadApp.controller("typesCtrl", function($scope){
+notepadApp.controller("typesCtrl", ["$scope", "$http", "log", function($scope, $http, log){
 	$scope.types = BD["tipos"];
-});
+	
+	//if (log.isLogged){
+		$http.get("req/hack.php", {params: {query: "SELECT * FROM tipo_registo WHERE userid=43"}}).success(function(data){
+			console.log(data);
+			if (data.length > 0){
+				$scope.types = data;
+			} else{
+				$scope.types = BD["tipos"];
+			}
+		});
+	//}
+}]);
 
+notepadApp.controller("recordCtrl", ["$scope", "$http", "log", function($scope, $http, log){
+	$scope.records = [];
+	
+	//if (log.isLogged){
+		$http.get("req/hack.php", {params: {query: "SELECT * FROM ist178013.tipo_registo WHERE userid=43 AND ativo=1"}}).success(function(data){
+			console.log(data);
+			if (data.length > 0){
+				$scope.records = data;
+			} else{
+				$scope.records = [];
+			}
+		});
+	//}
+}]);
 
 
 
